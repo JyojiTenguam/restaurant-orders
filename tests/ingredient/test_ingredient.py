602ca49f2d7bb015dc70af8b2d08c21484d28d8d
JyojiTenguam <j.y.oji@hotmail.com>
2024-10-16 06:08:57 -0300

    fix: def test_ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,37 +1,31 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 from src.models.ingredient import Restriction
 
+
 # Req 1
-# Testa a instância da classe Ingredient
-# e verifica os atributos name e restrictions
-ingredient = Ingredient("queijo mussarela")
-assert ingredient.name == "queijo mussarela"
-assert ingredient.restrictions == {
-    Restriction.LACTOSE,
-    Restriction.ANIMAL_DERIVED
-}
+def test_ingredient():
+    # Testa a instância da classe Ingredient
+    # e verifica os atributos name e restrictions
+    ingredient = Ingredient("queijo mussarela")
+    assert ingredient.name == "queijo mussarela"
+    assert ingredient.restrictions == {
+        Restriction.LACTOSE, Restriction.ANIMAL_DERIVED
+    }
 
-# Testa a representação textual (__repr__) do ingrediente
-ingredient = Ingredient("queijo mussarela")
-assert repr(ingredient) == "Ingredient('queijo mussarela')"
+    # Testa a representação textual (__repr__) do ingrediente
+    ingredient = Ingredient("queijo mussarela")
+    assert repr(ingredient) == "Ingredient('queijo mussarela')"
 
-# Testa a igualdade (__eq__) entre dois ingredientes iguais e diferentes
-ingredient1 = Ingredient("queijo mussarela")
-ingredient2 = Ingredient("queijo mussarela")
-ingredient3 = Ingredient("bacon")
-assert ingredient1 == ingredient2
-assert ingredient1 != ingredient3
+    # Testa a igualdade (__eq__) entre dois ingredientes iguais e diferentes
+    ingredient1 = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("queijo mussarela")
+    ingredient3 = Ingredient("bacon")
+    assert ingredient1 == ingredient2
+    assert ingredient1 != ingredient3
 
-# Testa o hash (__hash__) entre dois ingredientes iguais e diferentes
-ingredient1 = Ingredient("queijo mussarela")
-ingredient2 = Ingredient("queijo mussarela")
-ingredient3 = Ingredient("bacon")
-assert hash(ingredient1) == hash(ingredient2)
-assert hash(ingredient1) != hash(ingredient3)
-
-# Testa se as restrições alimentares do ingrediente são populadas corretamente
-ingredient = Ingredient("bacon")
-assert ingredient.restrictions == {
-    Restriction.ANIMAL_MEAT,
-    Restriction.ANIMAL_DERIVED
-}
+    # Testa o hash (__hash__) entre dois ingredientes iguais e diferentes
+    ingredient1 = Ingredient("queijo mussarela")
+    ingredient2 = Ingredient("queijo mussarela")
+    ingredient3 = Ingredient("bacon")
+    assert hash(ingredient1) == hash(ingredient2)
+    assert hash(ingredient1) != hash(ingredient3)
